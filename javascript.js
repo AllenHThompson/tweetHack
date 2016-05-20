8ac1697f3346cafd9f5a77869d56623c0c2fc087
Allen Thompson <allenhthompson1@gmail.com>
2016-05-20 09:07:09 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+/*
+interation 1:use phonetic concatination to take a paragraph not essay, and shorten by removing vowels
+iteration 2: if too long, run again
+iteration 3: if too long, back to user and say too long, shortern your paragraph
+
+*/
